5d8cc117ac069d72c3f55dc15e5c9b1a6828d76b
SloBaLiNuxAdd <165171813+slobam11@users.noreply.github.com>
2025-09-04 14:42:44 +0200

    Add files via upload

---
+++ An end-to-end demo project that shows ho.rb
@@ -0,0 +1,42 @@
+An end-to-end demo project that shows how to build a local **SRE lab**:
+Vagrant VM (Ubuntu 22.04, VirtualBox)
+
+The goal is to have a fully automated environment with a single `vagrant up`.
+
+---
+
+## Architecture
+
+1. Host OS (Windows/Linux/macOS)
+2. Vagrant + VirtualBox → Ubuntu 22.04 VM
+3. Docker engine + Minikube + kubectl + helm
+4. NGINX app deployed on Kubernetes
+5. Monitoring via **kube-prometheus-stack** (Prometheus + Grafana)
+
+---
+
+## Prerequisites
+
+- [Oracle VirtualBox](https://www.virtualbox.org/)
+- [Vagrant](https://www.vagrantup.com/downloads)
+- Git
+- Minimum **6 GB RAM** and **4 vCPUs** available on the host
+
+---
+
+## Getting Started
+
+```bash
+# Clone the repo
+git clone https://github.com/<your-user>/sre-mini-project.git
+cd sre-mini-project
+
+# Start VM
+vagrant up
+
+# Access the VM
+vagrant ssh
+
+# Check Kubernetes resources
+cd /vagrant
+make status
